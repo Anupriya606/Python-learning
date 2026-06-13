22202fe3c38f56d8c2a11e9ccf0a88a8e92d4dca
Anu Priya Singh <mahianupriya5@gmail.com>
2026-06-13 18:10:17 +0530

    Added basic python problem

---
+++ PythonFromScratch/Practice_Basic_Problems.py
@@ -0,0 +1,17 @@
+# Wap to input two numbers and print the sum
+
+# a=int(input("Enter first number: ", ))
+# b=int(input("Enter second number: ", ))
+# sum=a+b
+# print(sum)
+
+# Wap to input side of a sqaure and print its area
+# side=int(input("Enter side of a sqaure: ", ))
+# area=side*side
+# print("Area of a Sqaure is: ", area)
+
+# Wap to input two floating numbers and print the average of these numbers
+num1=float(input("Enter number 1: ", ))
+num2=float(input("Enter number 2: ", ))
+avg=(num1+num2)/2
+print("Average of two number: ",avg)
