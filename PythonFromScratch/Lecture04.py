ad732d52de169faa8b4d626e614bce4d6faa0135
Anu Priya Singh <mahianupriya5@gmail.com>
2026-06-14 13:44:51 +0530

    Check whether a number is multiple of 7

--- PythonFromScratch/Lecture04.py
+++ PythonFromScratch/Lecture04.py
@@ -42,17 +42,3 @@ list1=[67,90,100]
 # list1.pop(1)
 # print(list1)
 
-# Poll question
-name="Programming"
-print(len(name))
-
-num1= int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-num3= int(input("Enter third number: "))
-
-if num1 >= num2 and num1 >= num3:
-    print("Greatest number is:", num1)
-elif num2 >= num1 and num2 >= num3:
-    print("Greatest number is:", num2)
-else:
-    print("Greatest number is:", num3)
