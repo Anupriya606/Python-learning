57e8314ee3a322bb7506c2e7b26cc1ad15af15a0
Anu Priya Singh <mahianupriya5@gmail.com>
2026-06-15 18:21:41 +0530

    Python list practice- favourite movies

---
+++ Lectures/lecture05.py
@@ -0,0 +1,8 @@
+# TUPLES- Tuples are ordered,immutable collection. once created,their elements cannot be changed.
+# tup=(2,6,7,8)
+tup=(1,4,8,10,9,10)
+print(tup)
+print(type(tup))
+# tuple methods
+print(tup.index(10))
+print(tup.count(10))
