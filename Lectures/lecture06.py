cb93a67d821f0cadb5e6d7c218e3da82d8dc3af0
Anu Priya Singh <mahianupriya5@gmail.com>
2026-06-21 11:58:29 +0530

    Function to check whether a number is even or odd

--- Lectures/lecture06.py
+++ Lectures/lecture06.py
@@ -1,5 +1,40 @@
 # Set- itself it is mutable but the items that we write in set cannot be changed.
-set1={1,2,3,4,5}
-set2={1,2,3,4,5,"hello"}
-print(type(set1))
-print(type(set2))
+# Set is unordered
+# If the value is repeated inside the set, it will only take unique value and ignore the rest.
+# set1={1,2,3,4,5}
+# set2={1,2,3,4,5,"hello"}
+# print(type(set1))
+# print(type(set2))
+
+# set3=set()   #empty set
+# print(set3)
+
+# # Methods of set
+# set3.add(1) #add is used to add elements in set 
+# set3.add(10)
+# print(set3)
+
+# # set3.remove(10) #remove- used to remove elements from set
+# # print(set3)
+
+# # set3.clear() #clear-used to empty the set
+# # print(set3)
+
+# set3.pop() # removes any element from the set
+# print(set3)
+
+# set1={1,2,2,6,8,3,4,5}
+# set2={6,7,8,9,10}
+# final_set=set1.union(set2) #union of two set
+# # Union ignores repetative words
+# print(final_set)
+
+# final_set=set1.intersection(set2) 
+# print(final_set)
+
+
+# Program
+# set1={"python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C"}
+# print(len(set1))
+
+
---
+++ Lectures/lecture07.py
@@ -0,0 +1,20 @@
+# def cal_sum(a,b):
+#     sum=a+b
+#     print(sum)
+#     return sum
+# cal_sum(10,20)
+
+# wap to print elements of list in a single line
+# wap (function) to calculate factorial of a number
+# wap to convert usd to inr
+
+# Python program using function to check whether a number is even or odd:
+def check_num(num):
+    if num % 2 == 0:
+        print("Number is Even")
+    else:
+        print("Number is Odd")
+
+n = int(input("Enter  number: "))
+
+check_num(n)
