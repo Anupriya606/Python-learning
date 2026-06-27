0771be6449a7821abd2576efa6f4c41c575a6d50
Anu Priya Singh <mahianupriya5@gmail.com>
2026-06-27 18:11:20 +0530

    List vs Array

---
+++ Lectures/numpy01.py
@@ -0,0 +1,10 @@
+import numpy as np
+my_list=[1,2,3,4,5]
+print(my_list)
+print(type(my_list))
+
+#1. In array elements are stored in a memory and storage is continuos whereas storage in list are not contnious
+# 2. 
+array=np.array([1,2,3,4,5])
+print(array)
+print(type(array))
